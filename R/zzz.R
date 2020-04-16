0ab79c3498fdfeb5371909e32dcc718e05654ace
Emma Cliffe <cspehj@bath.ac.uk>
2020-04-16 13:34:35 +0100

    Trying to load bookdown as can't import needed

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,6 @@ NULL
 .onLoad = function(lib, pkg) {
   #For user created theorems we cannot register them by name as in the above
   #The below uses an engine based on block2
+  library(bookdown)
   register_eng_math('newtheorem', eng_newtheorem)
 }
