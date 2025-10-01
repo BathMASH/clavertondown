daeaead408a9d6c0809b2f642cc5577cf078c88b
Emma Cliffe <e.h.cliffe@gmail.com>
2025-10-01 15:44:50 +0100

    There is an issue with alt tags in ebook format, it appears unresolvable at the preent moment, and it is unclear what the impact is as the intended alt text is in the title and the caption is in the alt text. It could be that a screenreader interacting with an ebook will read both anyway. So just added note about this.

--- R/ebook.R
+++ R/ebook.R
@@ -54,6 +54,9 @@ move_output = function(output) {
 
 process_markdown = function(input_file, from, pandoc_args, global, new_theorems, number_by, style_with, classify_as, to_md = output_md()) {
   intermediate_html = with_ext(input_file, 'tmp.html')
+  #You can stop the removal of the intermediate format by uncommenting the below
+  #But keep in mind this is used for working out adjustments to the markdown only
+  #We are not going via html
   on.exit(file.remove(intermediate_html), add = TRUE)
   rmarkdown::pandoc_convert(
     input_file, 'html', from, intermediate_html, TRUE,
@@ -63,12 +66,14 @@ process_markdown = function(input_file, from, pandoc_args, global, new_theorems,
   x = clean_names(x)
   x = bookdown:::clean_html_tags(x)
   #Need to do the next thing or the newtheorems will not be picked up in the label list
+  #But this is not actually changing anything else, x is being used only the create figs 
   x = resolve_new_theorems(x, global = !number_sections, new_theorems, number_by)
   figs = parse_fig_labels(x, global, new_theorems, number_by)
-  
+
   # resolve cross-references and update the Markdown input file
   content = read_utf8(input_file)
   content = clean_names(content)
+  #This works because by this point the theorems are in html
   content = resolve_new_theorems(content, global = !number_sections, new_theorems, number_by)
   i = xfun::prose_index(content)
   content[i] = resolve_refs_md(content[i], c(figs$ref_table, bookdown:::parse_section_labels(x)), to_md)
@@ -81,6 +86,7 @@ process_markdown = function(input_file, from, pandoc_args, global, new_theorems,
 
   # We always want to turn off the italics, this isn't really needed to turn off the italics as they aren't carried through but it is needed to change
   # the styling to the non-italics style so that the right css kicks in. 
+  #This works because the theorems are by this point in html
   content = fix_classifications(content, classify_as)
   content = remove_colours(content, style_with["colouroff"][[1]])
   content = remove_italics(content, TRUE)  
@@ -93,6 +99,13 @@ process_markdown = function(input_file, from, pandoc_args, global, new_theorems,
     s = bookdown:::protect_math_env(s)
     content[i] = s
   }
+
+  #At this point we are in markdown and about to process directly to ebook, which we cannot post-process
+  #The result will be a format in which the caption and the alternative text of an image are the same and
+  #the intended alternative text will end up in the title. There appears to be no solution to this.
+  #Does it actually matter? Will a screenreader, reading an ebook, read the title and alternative text?
+  #I actually have no idea... and no immediate way to test this. So I will leave this unresolved for now. 
+
   write_utf8(content, input_file)
 }
 
